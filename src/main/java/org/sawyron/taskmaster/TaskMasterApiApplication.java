1e6da6cb8bb956e1012080e532961032a9fb85ca
Sawyron <s4pakvv@gmail.com>
2024-10-31 19:06:24 +0700

    initial commit

---
+++ src/main/java/org/sawyron/taskmaster/TaskMasterApiApplication.java
@@ -0,0 +1,12 @@
+package org.sawyron.taskmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMasterApiApplication.class, args);
+	}
+}
