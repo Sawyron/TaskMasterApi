760f1e03fc32bb68dfc8b17864e14207b4a656a2
Sawyron <s4pakvv@gmail.com>
2024-10-31 19:09:13 +0700

    add default controller advice

---
+++ src/main/java/org/sawyron/taskmaster/advices/DefaultAdvice.java
@@ -0,0 +1,26 @@
+package org.sawyron.taskmaster.advices;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class DefaultAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        e -> Optional.ofNullable(e.getDefaultMessage()).orElse("value is incorrect")));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
